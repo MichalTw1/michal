f9c6dc2e99527ce4995ebcdef39b8d76a54d4779
MichalTw1 <michaltw26@gmail.com>
2025-12-18 19:31:40 +0100

    Dodanie zadań po uwzględnieniu pycodestyle

--- models.py
+++ models.py
@@ -1,11 +1,14 @@
 from sqlalchemy import create_engine, Integer, String, Float
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
+
 db_url = "sqlite:///movies_data.db"
 engine = create_engine(db_url)
 
+
 class Base(DeclarativeBase):
     pass
+
 
 class Movie(Base):
     __tablename__ = 'movies'
@@ -13,11 +16,13 @@ class Movie(Base):
     title: Mapped[str] = mapped_column(String)
     genres: Mapped[str] = mapped_column(String)
 
+
 class Link(Base):
     __tablename__ = 'links'
     movieId: Mapped[int] = mapped_column(primary_key=True)
     imdbId: Mapped[str] = mapped_column(String)
     tmdbId: Mapped[str] = mapped_column(String)
+
 
 class Rating(Base):
     __tablename__ = 'ratings'
@@ -27,6 +32,7 @@ class Rating(Base):
     rating: Mapped[float] = mapped_column(Float)
     timestamp: Mapped[int] = mapped_column(Integer)
 
+
 class Tag(Base):
     __tablename__ = 'tags'
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
@@ -34,6 +40,7 @@ class Tag(Base):
     movieId: Mapped[int] = mapped_column(Integer)
     tag: Mapped[str] = mapped_column(String)
     timestamp: Mapped[int] = mapped_column(Integer)
+
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
